634a33c8c8401c8ddeaa5a06335d11a2f834c1ad
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-09 13:29:32 -0500

    build: seed-data.sh

---
+++ seed_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf oneleftfootapi/migrations
+rm db.sqlite3
+python3 manage.py makemigrations oneleftfootapi
+python3 manage.py migrate
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+python3 manage.py loaddata dance_users
+python3 manage.py loaddata partners
+python3 manage.py loaddata dance_types
+python3 manage.py loaddata skill_level
+python3 manage.py loaddata dance_type_join
+python3 manage.py loaddata days
+python3 manage.py loaddata availability
+
+
