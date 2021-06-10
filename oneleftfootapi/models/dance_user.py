26853387fee5c324d4f7edbe0a0bbed5f7d2aefe
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-10 09:18:34 -0500

    Build: trying to get an upload connection to cloudinary operational

--- oneleftfootapi/models/dance_user.py
+++ oneleftfootapi/models/dance_user.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class DanceUser(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=500)
-    img = models.ImageField
+    img = models.ImageField(upload_to="")
