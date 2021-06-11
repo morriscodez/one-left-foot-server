08eaf117faf23e6aea89138f0045f5bedb81e2cf
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-11 13:31:47 -0500

    Feat: CRUD working for dance type join table

--- oneleftfootapi/views/__init__.py
+++ oneleftfootapi/views/__init__.py
@@ -3,5 +3,6 @@ from .dance_user import DanceUserView
 from .dance_type import DanceTypeView
 from .skill_level import SkillLevelView
 from .days import DayView
-from . availability import AvailabilityView
+from .availability import AvailabilityView
 from .partners import PartnerView
+from .dance_type_join import DanceTypeJoinView
