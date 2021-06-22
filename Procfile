e2d98bc943409e5e2e2fde3360cb5bbb82c572a8
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-22 14:17:55 -0500

    added files for heroku, updated settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn oneleftfoot.wsgi --log-file -
--- oneleftfoot/settings.py
+++ oneleftfoot/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 import cloudinary
+import os
+import django_on_heroku
 
 env = environ.Env()
 environ.Env.read_env()
@@ -29,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -155,7 +157,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+django_on_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
