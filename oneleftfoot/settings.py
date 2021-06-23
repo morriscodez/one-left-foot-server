7087ac3fb4bbd98921c6d407be0190b2392a9374
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-23 13:01:01 -0500

    removed trailing slash from cors header

--- oneleftfoot/settings.py
+++ oneleftfoot/settings.py
@@ -76,7 +76,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://oneleftfoot-nss.herokuapp.com/'
+    'https://oneleftfoot-nss.herokuapp.com'
 )
 
 MIDDLEWARE = [
