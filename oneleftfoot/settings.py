8d9ee2d511f6c036b72cc5e45c50d976bb8dad7a
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-23 11:36:52 -0500

    fix: added new heroku client url to cors whitelist

--- oneleftfoot/settings.py
+++ oneleftfoot/settings.py
@@ -75,7 +75,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://oneleftfoot-nss.herokuapp.com/'
 )
 
 MIDDLEWARE = [
